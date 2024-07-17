f047a81682fd8b67e907171a0718fddac6383e7d
Swyckoff1939 <sean@houseofwombats.com>
2024-07-17 18:00:42 -0400

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,30 +1,80 @@
-import React, { useContext, useState } from 'react';
-import { AppContext } from '../context/AppContext';
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+
+//define max budget
+const BUDGET_MAX_VALUE = 20000;
 
 const Budget = () => {
-    const { budget, totalExpenses, currency } = useContext(AppContext);
-    const [newBudget, setNewBudget] = useState(budget);
-    const handleBudgetChange = (event) => {
-        if(event.target.value > 20000) {
-            alert("The budget cannot exceed 20,000. Setting it to 20,000");
-            setNewBudget(20000);
-            return;
-        }
+  //get global state from redux state
+  const { budget, expenses, currency, dispatch } = useContext(AppContext);
 
-        if(event.target.value < totalExpenses){
-            alert("The budget cannot be below the Spent so far:[" + totalExpenses + "]. Setting the budget to the minimum.");
-            setNewBudget(totalExpenses);
-            return;
+  //get total expenses
+  const totalExpenses = expenses.reduce((total, item) => {
+    return (total += item.cost);
+  }, 0);
 
-        }
+  //handle increase or decrease budget
+  const onChangeBudgetHandler = (event) => {
+    //convert input into number type
+    const enteredValue = Number(event.target.value);
 
-        setNewBudget(event.target.value);
+    // check if the entered value is a number
+    if (Number.isNaN(enteredValue)) {
+      alert("Please enter a valid number.");
+      return;
     }
-    return (
-<div className='alert alert-secondary'>
-<span>Budget: {currency}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
-    );
+
+    // check if the entered value is an integer number
+    if (!Number.isInteger(enteredValue)) {
+      alert("Please enter an integer number.");
+      return;
+    }
+
+    // check if the budget value < total expenses
+    if (enteredValue < totalExpenses) {
+      alert(
+        "The value of the buget can't be lower than the expenses value " +
+          currency +
+          totalExpenses
+      );
+    } else {
+      if (enteredValue > BUDGET_MAX_VALUE) {
+        alert("Please enter a value less that " + BUDGET_MAX_VALUE);
+        return;
+      }
+      //action dispatch function, sent type and payload to reducer
+      dispatch({
+        type: "SET_BUDGET",
+        payload: enteredValue,
+      });
+    }
+  };
+
+  return (
+    <div
+      className="alert alert-secondary"
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
+      <div>
+        <label htmlFor="budget"> Budget:</label>
+      </div>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <span>{currency}</span>
+        <input
+          required="required"
+          type="number"
+          id="budget"
+          value={budget}
+          step="10"
+          onChange={onChangeBudgetHandler}
+        ></input>
+      </div>
+    </div>
+  );
 };
+
 export default Budget;
